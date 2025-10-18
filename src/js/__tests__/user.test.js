3fa6d8bbc78fc10233fa3827d649141dbdf5b4e3
Yar <yarik.seredkin@gmail.com>
2025-10-18 20:44:54 +0500

    Обновлен user.test

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -1,4 +1,4 @@
-import { loadUser } from '../user';
+import { loadUser, saveUser } from '../user';
 import { httpGet } from '../http';
 
 jest.mock('../http');
@@ -13,4 +13,19 @@ test('should call loadUser once', () => {
   const response = loadUser(1);
   expect(response).toEqual({});
   expect(httpGet).toHaveBeenCalledWith('http://server:8080/users/1');
+});
+
+// Тест для saveUser
+describe('Тестирование функции saveUser', () => {
+
+  test('Должен выбрасывать ошибку Unimplemented', () => {
+    const user = {
+      id: 1,
+      name: 'Test User',
+      health: 50
+    };
+  
+    expect(() => saveUser(user))
+      .toThrow('Unimplemented');
+  });
 });
