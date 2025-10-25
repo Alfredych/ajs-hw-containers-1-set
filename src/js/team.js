ff2b39beca619cdf2d749a9539395cf18a78ff3e
Yar <yarik.seredkin@gmail.com>
2025-10-25 11:44:16 +0500

    Добавлен team.js

---
+++ src/js/team.js
@@ -0,0 +1,40 @@
+// Класс Персонажи
+class Character {
+  constructor(name, level, state = 'healthy') {
+    this.name = name;
+    this.level = level;
+    this.state = state;
+  }
+}
+
+
+// Класс Команда
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  // Метод добавления персонажа в команду
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Персонаж уже добавлен в команду');
+    }
+    this.members.add(character);
+  }
+
+// Метод добавления нескольких персонажей в команду, исключая их дублирование и не генерируя ошибки
+  addAll(...characters) {
+    characters.forEach(character => {
+      if (!this.members.has(character)) {
+        this.members.add(character);
+      }
+    });
+  }
+
+// Конвертируем Set персонажей в массив
+  toArray() {
+    return Array.from(this.members);
+  }
+}
+
+module.exports = { Team, Character };
